02cfa8216f7ba709ab363d42523066149b3043d3
mhassan24 <hassanbarlas50@gmail.com>
2022-01-23 19:45:39 +0500

    first commit

---
+++ precache-manifest.c65cac9ea013171eef9e6e516b087348.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b9c09895569865a9ff0edae9c6ecafc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6a1346cf1a4675d3f57",
+    "url": "/static/css/main.af87bd13.chunk.css"
+  },
+  {
+    "revision": "0de4d919a3e30fae6465",
+    "url": "/static/js/2.5b937fd0.chunk.js"
+  },
+  {
+    "revision": "e6a1346cf1a4675d3f57",
+    "url": "/static/js/main.67a29cac.chunk.js"
+  },
+  {
+    "revision": "36f1760ce38cabc09d4c",
+    "url": "/static/js/runtime-main.5df1f543.js"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/AI.e79c6392.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "9d46ce8fe45895231b0864b1d00c65f0",
+    "url": "/static/media/codup.9d46ce8f.jpg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "ce6f105d88d775d7b6eff20ae0f1348b",
+    "url": "/static/media/kpk.ce6f105d.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
